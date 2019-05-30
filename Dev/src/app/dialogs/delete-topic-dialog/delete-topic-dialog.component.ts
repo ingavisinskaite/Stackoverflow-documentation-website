38b80ecd77fe7fe129127abe8e941ae45a40934a
ingavisinskaite <ingavisinskaite@gmail.com>
2019-05-30 11:57:46 +0300

    [Main view] added edit topic button

--- Dev/src/app/dialogs/delete-topic-dialog/delete-topic-dialog.component.ts
+++ Dev/src/app/dialogs/delete-topic-dialog/delete-topic-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppDataService } from '../../services/app-data.service';
 import { Subscription } from 'rxjs';
+import { MatDialogRef } from '@angular/material';
 
 
 @Component({
@@ -12,22 +13,27 @@ export class DeleteTopicDialogComponent implements OnInit {
 
   topicTitle: string;
 
-  constructor(private _appDataService: AppDataService) { }
+  constructor(private _appDataService: AppDataService,
+              public dialogRef: MatDialogRef<DeleteTopicDialogComponent> ) { }
 
   ngOnInit() {
   }
 
   public deleteTopic(id: number): void {
     this._appDataService.deleteTopic(id)
-    .then(data => {
-      console.log('Deleted topic:' + data);
-    });
+      .then(data => {
+        console.log('Deleted topic:' + data);
+      });
   }
 
   public downloadTopic(topicId: number): Subscription {
     return this._appDataService.getTopic(topicId).subscribe(topic => {
       this.topicTitle = topic.Title;
     });
+  }
+
+  public closeTopicDeleteDialog(): void {
+    this.dialogRef.close();
   }
 
 }
